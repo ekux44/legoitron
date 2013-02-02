aaa86df26e597fa13dedc2d4a720d560dd124ede
ekux44 <ekux44@gmail.com>
2013-02-01 20:32:42 -0600

    Overroad performAcitionCommand from librairy

--- android/Legoitron/src/com/kuxhausen/legoitron/LockDroid.java
+++ android/Legoitron/src/com/kuxhausen/legoitron/LockDroid.java
@@ -1,6 +1,8 @@
 package com.kuxhausen.legoitron;
 
+import com.lego.minddroid.BTCommunicator;
 import com.lego.minddroid.MINDdroid;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -34,5 +36,21 @@ public class LockDroid extends MINDdroid implements OnClickListener{
 		}
 	}
 
-
+    @Override
+    /**
+     * Does something special depending on the robot-type.
+     * @param buttonMode short, long or other press types.
+     */
+    public void performActionCommand(int buttonMode) {
+        
+      
+			// other robots: 180 degrees forth and back
+			int direction = (buttonMode == ACTION_BUTTON_SHORT ? 1 : -1);
+			sendBTCmessage(BTCommunicator.NO_DELAY, BTCommunicator.MOTOR_A, 
+			    75*direction*1, 0);
+			sendBTCmessage(500, BTCommunicator.MOTOR_A, 
+			    -75*direction*1, 0);
+			sendBTCmessage(1000, BTCommunicator.MOTOR_A, 0, 0);
+		
+    }
 }
